dfe84702f4f07f4b48c86c0d83a52ec1e2e08777
Francis Nguyen <OrdinaryNeumann@gmail.com>
2014-10-04 11:46:38 -0400

    Adding framework for attacks

---
+++ attack_stab.cpp
@@ -0,0 +1,7 @@
+#include "attack_stab.hpp"
+
+void AttackStab::update(float dt) {
+	for (RigidBody* collidedEnt : this->collided) {
+
+	}
+}
---
+++ attack_stab.hpp
@@ -0,0 +1,10 @@
+#ifndef ATTACK_STAB_HPP
+#define ATTACK_STAB_HPP
+#include "Attack.hpp"
+
+class AttackStab : public Attack {
+public:
+	void update(float dt);
+};
+
+#endif
