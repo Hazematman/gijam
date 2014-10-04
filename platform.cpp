3681dfeef8ed07a7e428d80b47f25de13339652c
Francis Nguyen <OrdinaryNeumann@gmail.com>
2014-10-03 20:41:30 -0400

    Adding platforms

---
+++ platform.cpp
@@ -0,0 +1,23 @@
+#include "platform.hpp"
+using namespace std;
+
+Platform::Platform(){
+	this->rb = RigidBody(10,100,0,0);
+	this->rb.moves = false;
+}
+
+void Platform::setSprite(string filename){
+	texture.loadFromFile(filename);
+	sprite.setTexture(texture);
+	sf::Vector2u spriteSize = sprite.getTexture()->getSize();
+	this->rb = RigidBody(rb.pos.x,rb.pos.y,spriteSize.x,spriteSize.y);
+	this->rb.moves = false;
+}
+
+void Platform::update(float dt){
+}
+
+void Platform::render(sf::RenderWindow &screen){
+	sprite.setPosition(rb.pos);
+	screen.draw(this->sprite);
+}
