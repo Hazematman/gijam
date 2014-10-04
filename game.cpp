8f0188fb6b4760d15d1602743edc739002aeaeb2
Lucas Fryzek <nercos7@gmail.com>
2014-10-04 18:16:48 -0400

    add nice floor

--- game.cpp
+++ game.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 PhysWorld *gworld;
 Player *gplayer;
+Floor f;
 
 bool Game::init(){
 	screen.create(sf::VideoMode(800,600), "Game");
-	this->addPlatform(10,300);
-	this->addPlatform(100,300);
+	//this->addPlatform(10,300);
+	//this->addPlatform(100,200);
 	this->addEnemy(120,100);
+	f.init(600,300);
+	f.pos = sf::Vector2f(100,400);
+	world.bodies.push_back(&f);
 	p.setSprite("./data/images/charsheet.png");
 	world.bodies.push_back(&p);
 	world.gravity = sf::Vector2f(0,GRAVITY);
@@ -56,6 +60,7 @@ void Game::update(float dt){
 
 void Game::render(){
 	gplayer->render(screen);
+	f.render(screen);
 	for (int i = 0; i < this->plats.size(); i++) {
 		Platform* plat = plats.at(i).get();
 		plat->render(screen);
@@ -69,7 +74,7 @@ void Game::render(){
 void Game::addPlatform(int x, int y) {
 	this->plats.push_back(unique_ptr<Platform>(new Platform()));
 	Platform *plat = ((Platform*) this->plats.back().get());
-	plat->setSprite("./data/images/DownArrow.png");
+	plat->setSprite("./data/images/platforms.png");
 	world.bodies.push_back(plat);
 	plat->pos = sf::Vector2f(x,y);
 }
--- platform.cpp
+++ platform.cpp
@@ -6,6 +6,9 @@ Platform::Platform(){
 	this->pos = sf::Vector2f(10,300);
 	this->moves = false;
 	this->tag = "platform";
+	this->body = sf::FloatRect(0,0,15*4,4*4);
+	this->sprite.setTextureRect(sf::IntRect(0,0,15,4));
+	this->sprite.setScale(4,4);
 }
 
 void Platform::update(float dt){
@@ -14,4 +17,19 @@ void Platform::update(float dt){
 void Platform::render(sf::RenderWindow &screen){
 	sprite.setPosition(pos);
 	screen.draw(this->sprite);
+}
+
+void Floor::init(int xsize, int ysize){
+	this->moves = false;
+	this->tag = "platform";
+	rect.setSize(sf::Vector2f(xsize, ysize));
+	body = sf::FloatRect(0,0, xsize, ysize);
+}
+
+void Floor::update(float dt){
+}
+
+void Floor::render(sf::RenderWindow &screen){
+	rect.setPosition(pos);
+	screen.draw(rect);
 }
